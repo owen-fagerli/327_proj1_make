6f18f5f688f0fcd9ac6367c152feeb9d17611eab
owen-fagerli <owen.fagerli.18@cnu.edu>
2021-02-06 11:53:23 -0800

    finished i think

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
 
-std::string func()
-{
-	//say hello
-	return "hello";
-}
 
 int main()
 {
